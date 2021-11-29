9a45c9ebac040d3ff2635d1e7357cc9419e9990f
rao-bushra21 <rao.bushra21@gmail.com>
2021-11-29 21:03:13 +0500

    first commit

---
+++ BasicMathsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicMathsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicMathsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4994c9b7-3603-473d-929b-61dba175cc45")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BasicMathsTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BasicMaths;
+namespace BasicMathsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMathsOperation bm = new BasicMathsOperation();
+                          double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+                      }  
+	        [TestMethod]  
+	    public void Test_SubstractMethod()
+        {
+            BasicMathsOperation bm = new BasicMathsOperation();
+                         double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+                     }  
+[TestMethod]  
+	    public void Test_DivideMethod()
+        {
+            BasicMathsOperation bm = new BasicMathsOperation();
+                       double res = bm.divide(10, 5);
+          Assert.AreEqual(res, 2);
+                   }  
+     [TestMethod]  
+	    public void Test_MultiplyMethod()
+        {
+            BasicMathsOperation bm = new BasicMathsOperation();
+                     double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+              }
+
+    }
+}
